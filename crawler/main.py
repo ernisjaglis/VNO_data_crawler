b6e6a8ec0fdcd5450c471e1871ae483b22225e4a
Ernestas Jaglinskis <ernestas.jaglinskis@gmail.com>
2023-12-18 21:57:05 +0200

    Coded function 'get_airport_url' Started coding function 'get_airport_data'
    file 'to_be_removed' will re removed completelly later

--- crawler/main.py
+++ crawler/main.py
@@ -1,126 +1,72 @@
 
-#failu formatavimas
-#poetry add black isort
-
-#pytest --cov
-#testavimo padengiamumui
-
-
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from queue import Queue
-from typing import Any
-from lxml.etree import HTML
 from requests import get
- 
+from lxml.etree import HTML
 
-# Global variables
-visited_pages = set()
-visited_articles = set()
-pages_queue = Queue()
-articles_queue = Queue()
- 
 
- 
-def query_to_url(query: str) -> str:
-    return f"https://www.flightradar24.com/data/airports/vno/departures{query}"
- 
- 
-BASE_URL = "https://www.lrytas.lt{}"
-DATE_FROM = "2021-01-01"
-TERM = "vakcinavimas"
- 
- 
-def get_articles(page: int) -> dict:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
-        "Accept": "application/json, text/plain, */*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Origin": "https://www.lrytas.lt",
-        "Connection": "keep-alive",
-        "Referer": "https://www.lrytas.lt/",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
-        "Sec-GPC": "1",
+# Defining function to find the url based on users selection
+def get_airport_url():
+    # Creating dictionary with links to airports info
+    airports_select = {
+        "City": ["Vilnius", "Doha", "Frankfurt"],
+        "General_info_url": ["https://www.flightradar24.com/data/airports/vno",
+                             "https://www.flightradar24.com/data/airports/doh",
+                             "https://www.flightradar24.com/data/airports/fra"],
+        "Arrival_url": ["https://www.flightradar24.com/data/airports/vno/arrivals",
+                        "https://www.flightradar24.com/data/airports/doh/arrivals",
+                        "https://www.flightradar24.com/data/airports/fra/arrivals"],
+        "Departure_url": ["https://www.flightradar24.com/data/airports/vno/departures",
+                          "https://www.flightradar24.com/data/airports/doh/departures",
+                          "https://www.flightradar24.com/data/airports/fra/departures"]
     }
-    params = {
-        "count": "12",
-        "kw_count": "12",
-        "order": "pubfromdate-",
-        "page": str(page),
-        "dfrom": DATE_FROM,
-        "q_text": TERM,
-    }
+
+    # Asking the user to type name of the city he is interested
+    city_input = input("Please enter the city you are interested in (Vilnius, Doha, Frankfurt): ").capitalize()
+
+    # Validating user input of city
+    if city_input not in airports_select["City"]:
+        print("Invalid city input. Please choose from Vilnius, Doha, or Frankfurt.")
+        return None
+
+    # Asking user type of information he needs
+    info_type_input = input("Enter the type of information you are interested in (General, Arrival, Departure): ").capitalize()
+
+    # Validating user input
+    if info_type_input not in ["General", "Arrival", "Departure"]:
+        print("Invalid information type input. Please choose from General, Arrival, or Departure.")
+        return None
+
+    # Getting the index of the selected city
+    city_index = airports_select["City"].index(city_input)
+
+    # Gettimg the URL based on user input
+    if info_type_input == "General":
+        return airports_select["General_info_url"][city_index]
+    elif info_type_input == "Arrival":
+        return airports_select["Arrival_url"][city_index]
+    elif info_type_input == "Departure":
+        return airports_select["Departure_url"][city_index]
+
+# Assigning selected by user details to url and naming it selected_url    
+selected_url = get_airport_url()
+print(selected_url) #can be commented, showing just for myself to see if process working
+
+
+
+# Defining function to get data from url which user selected
+def get_airport_data(url: selected_url):
+    #print(url)
     try:
-        return get(
-            "https://kolumbus-api.lrytas.lt/api_dev/fe/search/0/",
-            params=params,
-            headers=headers,
-        ).json()
-    except Exception:
-        print(f"Failed to get raw data for {page}")
- 
- 
-def process_articles_page(articles_page: dict[str, Any]) -> list[tuple[int, int, int]]:
-    return [
-        (
-            article["rating"]["patiko"],
-            article["rating"]["neblogai"],
-            article["rating"]["nepatiko"],
-        )
-        for article in articles_page["articles"]
-    ]
- 
- 
-def extract_data_from_article(page_html: str) -> tuple[int, int, int]:
-     tree = HTML(page_html)
-     return tuple(
-         int(element.strip())
-         for element in tree.xpath("//div[@class='LArticleEmotions__count']/text()")
-     )
- 
- 
-def process_page(page_url: str):
-    with lock:
-        print(page_url)
-    # global visited_pages, visited_articles, pages_queue, articles_queue
-    try:
-        response = get(page_url)
-        tree = HTML(response.text)
-        for page_link in tree.xpath(
-            "//a[contains(@class, 'LPagination__button')]/@href"
-        ):
-            full_page_url = "https://www.lrytas.lt" + page_link
-            if full_page_url not in visited_pages:
-                pages_queue.put(full_page_url)
-                visited_pages.add(full_page_url)
-        for article_link in tree.xpath("//a[@class='LPostContent__anchor']/@href"):
-            article_url = "https://www.lrytas.lt" + article_link
-            if article_url not in visited_articles:
-                visited_articles.add(article_url)
-                articles_queue.put(article_url)
-    except Exception as e:
-        print("Error retrieving url:", e)
- 
- 
-def main():
-    initial_url = query_to_url("rytas")
-    pages_queue.put(initial_url)
-    visited_pages.add(initial_url)
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = set()
-        while not pages_queue.empty() or futures:
-            # Submit new tasks from the queue
-            while not pages_queue.empty() and len(futures) < 10:
-                page = pages_queue.get()
-                future = executor.submit(process_page, page)
-                futures.add(future)
-            # Wait for the next future to complete
-            done = as_completed(futures, timeout=None)
-            for future in done:
-                futures.remove(future)
-    print("Finished processing all pages.")
- 
- 
-if __name__ == "__main__":
-    main()
+        response = get(url)
+        if 
+        
+
+        html_content = response.text
+        return html_content
+    except requests.RequestException as e:
+        print(f"Error fetching data from the webpage: {e}")
+        return None
+
+html_data = get_airport_data(selected_url)
+print(html_data) #can be commented, showing just for myself to see if process working
+
+
